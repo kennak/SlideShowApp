79e255eeaf3293e35ac61bd0fc4e8f6c7bf50cb8
kennak <knaka.home@gmail.com>
2026-06-06 14:32:58 +0900

    Initial Commit

---
+++ SlideShowApp/SlideShowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideShowAppApp.swift
+//  SlideShowApp
+//
+//  Created by kenichi nakamura on 2026/06/06.
+//
+
+import SwiftUI
+
+@main
+struct SlideShowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
